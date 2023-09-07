3bd244a19965f1a64bc3641ef3c4002b572d2199
sntcristian <52455931+sntcristian@users.noreply.github.com>
2023-09-07 15:48:23 +0200

    updates on ner notebooks

---
+++ scripts_eval/eval_nel.py
@@ -0,0 +1,88 @@
+import csv
+
+def compute_match(type, entity1, entity2):
+    start_pos1 = int(entity1["start_pos"])
+    end_pos1 = int(entity1["end_pos"])
+    start_pos2 = int(entity2["start_pos"])
+    end_pos2 = int(entity2["end_pos"])
+    if type == "exact":
+        if start_pos1==start_pos2 and end_pos1==end_pos2:
+            return True
+        else:
+            return False
+    if type == "relaxed":
+        if len(set(range(start_pos1,end_pos1)).intersection(set(range(start_pos2,end_pos2))))>0:
+            return True
+        else:
+            return False
+
+def eval_nel(path_data, path_results):
+    with open(path_data+"entities.csv", "r") as f1:
+        data = list(csv.DictReader(f1, delimiter=","))
+    with open(path_results+"output_nel.csv", "r") as f2:
+        model_result = list(csv.DictReader(f2, delimiter=","))
+    
+    tp = []
+    fp = []
+    fn = []
+    matches = []
+
+    for entity1 in data:
+        id1 = int(entity1["id"])
+        wb_id1 = entity1["value"].split(";")
+        if wb_id1==["NIL"]:
+            continue
+        else:
+            for entity2 in model_result:
+                id2 = int(entity2["id"])
+                wb_id2 = entity2["wb_id"]
+                if id2 == id1 and compute_match(type="relaxed", entity1=entity1, entity2=entity2)==True and wb_id2 in wb_id1:
+                    matches.append(entity1)
+                    tp.append(entity2)
+
+    for entity1 in data:
+        if entity1 not in matches:
+            fn.append(entity1)
+
+    for entity2 in model_result:
+        if entity2 not in tp:
+            fp.append(entity2)
+
+    precision = len(tp)/(len(tp)+len(fp))
+    recall = len(tp)/(len(tp)+len(fn))
+    f1 = (2*precision*recall)/(precision+recall)
+    with open(path_results+"result_nel.txt", "w") as output:
+        output.write("True Positives: "+str(len(tp))+"\n\n")
+        output.write("False Positives: "+str(len(fp))+"\n\n")
+        output.write("False Negatives: "+str(len(fn))+"\n\n")
+        output.write("Precision: "+ str(precision)+ "\n\n")
+        output.write("Recall: "+ str(recall)+ "\n\n")
+        output.write("F1: "+ str(f1)+"\n\n")
+    
+    
+    p_keys = matches[0].keys()
+    fp_keys = fp[0].keys()
+    n_keys = fn[0].keys()
+
+    tp_file = open(path_results+"tp_nel.csv", "w")
+    dict_writer = csv.DictWriter(tp_file, p_keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(matches)
+    tp_file.close()
+    
+    fp_file = open(path_results+"fp_nel.csv", "w")
+    dict_writer = csv.DictWriter(fp_file, fp_keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(fp)
+    fp_file.close()
+
+    fn_file = open(path_results+"fn_nel.csv", "w")
+    dict_writer = csv.DictWriter(fn_file, n_keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(fn)
+    fn_file.close()
+
+
+eval_nel(path_data="../data/", path_results="../results/mgenre_nel/")
+
+
---
+++ scripts_eval/eval_works.py
@@ -0,0 +1,84 @@
+import csv
+
+def eval_ner(path_data, path_model):
+    with open(path_data+"works.csv", "r") as f1:
+        data = list(csv.DictReader(f1, delimiter=","))
+    with open(path_model+"output.csv", "r") as f2:
+        model_result = list(csv.DictReader(f2, delimiter=","))
+    
+    tp = []
+    fp = []
+    fn = []
+    matches = []
+
+    for entity1 in data:
+        id1 = int(entity1["id"])
+        fuzzy_start = entity1["start_pos"].split("|")
+        fuzzy_end = entity1["end_pos"].split("|")
+        fuzzy_surface = entity1["surface"].split("|")
+        for start, end, surface in zip(fuzzy_start, fuzzy_end, fuzzy_surface):
+            start_pos1 = int(start)
+            end_pos1 = int(end)
+            surface1 = set(surface.split(" "))
+            for entity2 in model_result:
+                id2 = int(entity2["doc_id"])
+                start_pos2 = int(entity2["doc_start_pos"])
+                end_pos2 = int(entity2["doc_end_pos"])
+                surface2 = set(entity2["surface"].split(" "))
+                if id2 == id1 and len(set(range(start_pos1, end_pos1)).intersection(set(range(start_pos2, end_pos2))))>0 \
+                    and len(surface1.intersection(surface2))>0:
+                    matches.append(entity1)
+                    tp.append(entity2)
+                    break
+            break
+            
+    for entity1 in data:
+        if entity1 not in matches:
+            fn.append(entity1)
+
+    for entity2 in model_result:
+        if entity2 not in tp:
+            fp.append(entity2)
+
+    precision = len(tp)/(len(tp)+len(fp))
+    recall = len(tp)/(len(tp)+len(fn))
+    f1 = (2*precision*recall)/(precision+recall)
+
+
+
+    with open(path_model+"result.txt", "w") as output:
+        output.write("True Positives: "+str(len(tp))+"\n\n")
+        output.write("False Positives: "+str(len(fp))+"\n\n")
+        output.write("False Negatives: "+str(len(fn))+"\n\n")
+        output.write("Precision: "+ str(precision)+ "\n\n")
+        output.write("Recall: "+ str(recall)+ "\n\n")
+        output.write("F1: "+ str(f1)+"\n\n")
+    
+    
+    p_keys = tp[0].keys()
+    n_keys = fn[0].keys()
+
+    tp_file = open(path_model+"tp_ner.csv", "w")
+    dict_writer = csv.DictWriter(tp_file, p_keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(tp)
+    tp_file.close()
+    
+    fp_file = open(path_model+"fp_ner.csv", "w")
+    dict_writer = csv.DictWriter(fp_file, p_keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(fp)
+    fp_file.close()
+
+    fn_file = open(path_model+"fn_ner.csv", "w")
+    dict_writer = csv.DictWriter(fn_file, n_keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(fn)
+    fn_file.close()
+
+eval_ner(path_data="../data/", path_model="../results/ontonotes_works/")
+
+
+
+    
+
