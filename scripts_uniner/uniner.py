56770c3ed0e7c6be2798a6f652da4856136ba13e
sntcristian <52455931+sntcristian@users.noreply.github.com>
2023-09-11 15:14:22 +0200

    minfix uniner

--- scripts_uniner/uniner.py
+++ scripts_uniner/uniner.py
@@ -70,41 +70,43 @@ with open("../data/sentences.csv", "r", encoding="utf-8") as f:
 
 entity_types = {"Artwork"}
 pbar = tqdm(total=len(data))
-for sample in data:
-    output = []
-    text = sample["sentence"] # document with multiple sentences
-    doc_id = sample["id"] # document id
-    sentences = [s for s in sent_tokenize(text)] # document parsed into sentences
-    curr_pos = 0
-    for sentence_id, sentence in enumerate(sentences): # (sentence_id, sentence)
-        for _type in entity_types: # set of entity types
-            result_string = llm_chain.run({"input_text":sentence,"entity_type":_type})
-            result_lst = json.loads(result_string)
-            counter = 0
-            for ent in result_lst:
-                matches = re.finditer(ent, sentence)
-                spans = [(match.start(), match.end()) for match in matches]
-                for span in spans:
-                    if span[0]>=counter:
-                        output.append({
-                            "doc_id":doc_id,
-                            "sent_id":sentence_id,
-                            "doc_start_pos":curr_pos+span[0],
-                            "doc_end_pos":curr_pos+span[1],
-                            "sent_start_pos":span[0],
-                            "sent_end_pos":span[1],
-                            "surface":sentence[span[0]:span[1]],
-                            "type":_type
-                        })
-                        counter=span[0]
-                        break
-        curr_pos=curr_pos+len(sentence)+1
-    if len(output)>0:
-        keys = output[0].keys()
-        a_file = open("../results/uniner/output_"+doc_id+".csv", "w", encoding="utf-8")
-        dict_writer = csv.DictWriter(a_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(output)
-        a_file.close()
-        pbar.update(1)
+
+with torch.no_grad():
+    for sample in data:
+        output = []
+        text = sample["sentence"] # document with multiple sentences
+        doc_id = sample["id"] # document id
+        sentences = [s for s in sent_tokenize(text)] # document parsed into sentences
+        curr_pos = 0
+        for sentence_id, sentence in enumerate(sentences): # (sentence_id, sentence)
+            for _type in entity_types: # set of entity types
+                result_string = llm_chain.run({"input_text":sentence,"entity_type":_type})
+                result_lst = json.loads(result_string)
+                counter = 0
+                for ent in result_lst:
+                    matches = re.finditer(ent, sentence)
+                    spans = [(match.start(), match.end()) for match in matches]
+                    for span in spans:
+                        if span[0]>=counter:
+                            output.append({
+                                "doc_id":doc_id,
+                                "sent_id":sentence_id,
+                                "doc_start_pos":curr_pos+span[0],
+                                "doc_end_pos":curr_pos+span[1],
+                                "sent_start_pos":span[0],
+                                "sent_end_pos":span[1],
+                                "surface":sentence[span[0]:span[1]],
+                                "type":_type
+                            })
+                            counter=span[0]
+                            break
+            curr_pos=curr_pos+len(sentence)+1
+        if len(output)>0:
+            keys = output[0].keys()
+            a_file = open("../results/uniner/output_"+doc_id+".csv", "w", encoding="utf-8")
+            dict_writer = csv.DictWriter(a_file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(output)
+            a_file.close()
+            pbar.update(1)
 
