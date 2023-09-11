880eda07e4e13ed19d3614e92bbc343b1db6240b
sntcristian <52455931+sntcristian@users.noreply.github.com>
2023-09-11 15:34:59 +0200

    uniner results

---
+++ scripts_eval/combine_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+
+combined_df = pd.DataFrame()
+folder_path = '../results/uniner_works/'  # Change this to your folder path
+
+for filename in os.listdir(folder_path):
+    if filename.endswith(".csv"):
+        file_path = os.path.join(folder_path, filename)
+        df = pd.read_csv(file_path)
+        combined_df = combined_df.append(df, ignore_index=True)
+
+combined_df.to_csv(folder_path+'/output.csv', index=False)
