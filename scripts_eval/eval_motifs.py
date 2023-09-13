3ff9b97ecde157359b1204a0de36f9219401d917
sntcristian <52455931+sntcristian@users.noreply.github.com>
2023-09-13 16:25:27 +0200

    motif recognition results

--- scripts_eval/eval_motifs.py
+++ scripts_eval/eval_motifs.py
@@ -12,30 +12,23 @@ def eval_ner(path_data, path_model):
     matches = []
 
     for entity1 in data:
-        if entity1["type"]=="STATE":
-            continue
-        else:
-            id1 = int(entity1["id"])
-            start_pos1 = int(entity1["start_pos"])
-            end_pos1 = int(entity1["end_pos"])
-            surface1 = entity1["surface"]
-            surface_tokens = set(surface1.split(" "))
-            for entity2 in model_result:
-                id2 = int(entity2["doc_id"])
-                start_pos2 = int(entity2["doc_start_pos"])
-                end_pos2 = int(entity2["doc_end_pos"])
-                surface2 = set(entity2["surface"].split(" "))
-                if id2 == id1 and len(set(range(start_pos1, end_pos1)).intersection(set(range(start_pos2, end_pos2))))>0 \
-                    and len(surface_tokens.intersection(surface2))>0:
-                    if len(tp)>0 and tp[-1]["surface"]==entity2["surface"]:
-                        continue
-                    else:
-                        tp.append(entity2)#
-                        tp[-1]["matched_sf"]=entity1["surface"]
-                        matches.append(entity1)
+        id1 = entity1["id"]
+        start_pos1 = int(entity1["start_pos"])
+        end_pos1 = int(entity1["end_pos"])
+        for entity2 in model_result:
+            id2 = entity2["doc_id"]
+            start_pos2 = int(entity2["doc_start_pos"])
+            end_pos2 = int(entity2["doc_end_pos"])
+            if id2 == id1 and len(set(range(start_pos1, end_pos1)).intersection(set(range(start_pos2, end_pos2))))>1:
+                if len(tp)>0 and tp[-1]["surface"]==entity2["surface"] and tp[-1]["doc_start_pos"]==str(start_pos2):
+                    print(entity2.values(), entity1.values())
+                    continue
+                else:
+                    tp.append(entity2)
+                    matches.append(entity1)
             
     for entity1 in data:
-        if entity1 not in matches and not entity1["type"]=="STATE":
+        if entity1 not in matches and entity1["type"]!="STATE":
             fn.append(entity1)
 
     for entity2 in model_result:
@@ -58,7 +51,6 @@ def eval_ner(path_data, path_model):
     
     p_keys = tp[0].keys()
     n_keys = fn[0].keys()
-    print(n_keys)
     tp_file = open(path_model+"tp_ner.csv", "w")
     dict_writer = csv.DictWriter(tp_file, p_keys)
     dict_writer.writeheader()
